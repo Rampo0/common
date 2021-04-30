db8e8347d92a6c7607d6131e886fca2aefeaee5a
Ammar Dwi Anwari <36811859+Rampo0@users.noreply.github.com>
2021-04-30 12:37:24 +0700

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -17,17 +17,24 @@ export const currentUser = ( req : Request , res : Response , next : NextFunctio
     let token: string = "";
 
     if(authorization){
-        token = (authorization!.split(" "))[1]
+        try {
+            token = (authorization!.split(" "))[1]
+        } catch (err) { }
     }
 
     if(token){
-        const payload = jwt.verify(token ,  process.env.JWT_KEY!) as UserPayload;
-        req.currentUser = payload;
+        try {
+            const payload = jwt.verify(token ,  process.env.JWT_KEY!) as UserPayload;
+            req.currentUser = payload;
+        } catch (err) {}
+      
     }
 
     if(req.session?.jwt){
-        const payload = jwt.verify(req.session.jwt ,  process.env.JWT_KEY!) as UserPayload;
-        req.currentUser = payload;
+        try {
+            const payload = jwt.verify(req.session.jwt ,  process.env.JWT_KEY!) as UserPayload;
+            req.currentUser = payload;
+        } catch (err) {}
     }
 
     next();
